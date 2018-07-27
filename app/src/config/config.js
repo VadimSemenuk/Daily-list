b2ddae50b6310ee2d1c1a94ae9ae805dbc512957
Vadim Semenyk <vadim54787@gmail.com>
2018-07-27 23:03:44 +0300

    db config moved to common config

--- app/src/config/config.js
+++ app/src/config/config.js
@@ -1,5 +1,12 @@
 export default {
     appId: "com.dailylist.vadimsemenyk",
     apiURL: "http://localhost:3001/api",
-    LSTokenKey: "com.dailylist.vadimsemenyk.token"
+    LSTokenKey: "com.dailylist.vadimsemenyk.token",
+    db: {
+        name: 'com.mamindeveloper.dailylist.db',
+        version: '1.0',
+        displayname: 'Daily list app database',
+        size: 200000,
+        location: 'default'
+    }
 }
--- app/src/db/config.js
+++
@@ -1,9 +0,0 @@
-export default {
-    db: {
-        name: 'com.mamindeveloper.dailylist.db',
-        version: '1.0',
-        displayname: 'Daily list app database',
-        size: 200000,
-        location: 'default'
-    }
-}
