f3ebc7a94bb91a2d28d7558866f93a085cb05899
Vadim Semenyk <vadim54787@gmail.com>
2018-08-06 21:42:48 +0300

    i18n en

--- app/src/i18n/en.js
+++ app/src/i18n/en.js
@@ -1,4 +1,67 @@
-export default {    
+export default {
+    // settings page
+    "settings": "Settings",
+    "default-notification": "Default notification turn on",
+    "view": "Display",
+    "interface": "Interface",
+    "data-sync": "Data synchronization",
+    "add-pass": "Add password",
+    "remove-pass": "Remove password",
+    "issues": "Bug fixing",
+    "about": "About program",
+  
+    // view settings page
+    "sort": "Sorting",
+    "time-sort": "Time sorting",
+    "time-add-sort": "By time sorting",
+    "view-direction": "Display direction",
+    "view-direction-asc": "Direct order",
+    "view-direction-desc": "Reverse order",
+    "fin-sort": "Move completed down",
+  
+    // interface page
+    "theme": "Theme",
+    "random-theme": "Random theme",
+    "font-size": "Font size",
+    "fast-add": "Qick add",
+
+    // password page
+    "password": "Password",
+    "invalid-password": "Invalid password",
+    "pass-in": "Enter password",
+    "not-equal-passwords": "Not equal passwords",
+    "no-space-password": "Space is forbidden in password field",
+    "min-symbols-password": "Enter at least 4 symbols",
+    "new-password": "Enter new password",
+    "repeat-password": "Enter your password again",
+
+    //troubleshooting page
+    "help": "Help",
+    "previous-version": "Previous version",
+    "no-notification": "Notifications don't comming",
+    "no-notification-a": "Move to app settings",
+    "no-notification-b": "Next move to notification settings",
+    "no-notification-c": "For permition 'Run in background' (name can be different) set to 'Allow'",
+
     // about page
-    "app-name": "Daily list",
-}
+    "app-name": "Diary",
+    "share-app": "Share app",
+    "resources": "Used resources",
+    "graphics": "Graphics",
+
+    // common
+    "save": "Save data",
+    "move": "Move to",
+    "star-app": "Rate app",
+    "internet-required": "Connect to the internet",
+
+    // share
+    "share-theme": "Diary",
+    "share-content": "Diary notes and notifications in conviniet form",
+
+    // fast-add field
+    "description": "Description",
+    
+    // notes list page
+    "no-content": "No notes yet"
+} 
