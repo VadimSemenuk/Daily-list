29ee55608dae23ca93a25fcd2133acb6ba897917
Vadim Semenyuk <vadim54787@gmail.com>
2018-09-07 19:39:21 +0300

    migration dev

--- app/src/db/migration/migrations/v16.js
+++ app/src/db/migration/migrations/v16.js
@@ -5,106 +5,104 @@ export default {
     name: "1.6",
 
     async run() {
-        await execureSQL(
-            `CREATE TABLE IF NOT EXISTS Migrations
-            (
-                id INTEGER PRIMARY KEY,
-                name TEXT                  
-            );`
-        );
-        
-        await execureSQL(`ALTER TABLE Settings RENAME TO Settings_OLD;`);
-        await execureSQL(`                           
-            CREATE TABLE IF NOT EXISTS Settings
-            (
-                defaultNotification INTEGER,
-                sort TEXT,
-                fastAdd INTEGER,
-                theme INTEGER,
-                password TEXT,    
-                fontSize INTEGER,
-                showMiniCalendar INTEGER,
-                notesShowInterval INTEGER,
-                lang TEXT,
-                calendarNotesCounter INTEGER
-            );
-        `);
-        await execureSQL(`
-            INSERT INTO Settings (defaultNotification, fastAdd, theme, password, fontSize) 
-            SELECT defaultNotification, fastAdd, colorTheme AS theme, password, fontSize 
-            FROM Settings_OLD;
-        `);
-        await execureSQL(`DROP TABLE Settings_OLD;`);
-        let sort = {
-            type: 1,
-            direction: 1,
-            finSort: 0
-        }
-        
-        let lang = navigator.globalization ? (await new Promise((resolve, reject) => navigator.globalization.getPreferredLanguage(resolve, reject))) : config.defaultLang;
-        if (lang) {
-            lang = lang.value || "en";
-        }
-        if (lang.indexOf("-") !== -1) {
-            lang = lang.split("-")[0];
-        }
-        await execureSQL(`
-            UPDATE Settings 
-            SET 
-                sort = ?, 
-                showMiniCalendar = ?,
-                notesShowInterval = ?,
-                lang = ?,
-                calendarNotesCounter = ?;`, 
-            [JSON.stringify(sort), 1, 1, lang, 1]
-        );
+        // await addMigrationsTable();
+        // await alterSettingsTable();
+        await alterTasksTable();
 
-        let msNow = +new Date();
-        await execureSQL(`ALTER TABLE Tasks RENAME TO Tasks_OLD;`);
-        await execureSQL(`                           
-            CREATE TABLE IF NOT EXISTS Tasks
+        await execureSQL(
+            `CREATE TABLE IF NOT EXISTS TasksRepeatValues
             (
-                id INTEGER PRIMARY KEY,
-                uuid TEXT,
-                title TEXT,
-                startTime INTEGER,
-                endTime INTEGER,
-                notificate INTEGER,
-                tag TEXT,
-                dynamicFields TEXT,
-                added INTEGER,
-                finished INTEGER DEFAULT 0,
-                isSynced INTEGER DEFAULT 0,
-                isLastActionSynced INTEGER DEFAULT 0,
-                lastAction TEXT,
-                lastActionTime INTEGER,
-                userId INTEGER,
-                repeatType INTEGER DEFAULT "no-repeat",
-                UNIQUE (uuid) ON CONFLICT REPLACE 
+                taskId INTEGER,
+                value INTEGER,
+                FOREIGN KEY(taskId) REFERENCES Tasks(id)
+            );`
+        )
+
+        async function addMigrationsTable () {
+            await execureSQL(
+                `CREATE TABLE IF NOT EXISTS Migrations
+                (
+                    id INTEGER PRIMARY KEY,
+                    name TEXT                  
+                );`
             );
-        `);
-        await execureSQL(`
-            INSERT INTO Settings (
-                id, 
-                uuid, 
-                title, 
-                startTime, 
-                endTime, 
-                notificate, 
-                tag, 
-                dynamicFields, 
-                added, 
-                finished, 
-                isSynced,
-                isLastActionSynced,
-                lastAction,
-                lastActionTime,
-                userId,
-                repeatType
-            ) 
+        }
+
+        async function alterSettingsTable () {
+            await execureSQL(`ALTER TABLE Settings RENAME TO Settings_OLD;`);
+            await execureSQL(`                           
+                CREATE TABLE IF NOT EXISTS Settings
+                (
+                    defaultNotification INTEGER,
+                    sort TEXT,
+                    fastAdd INTEGER,
+                    theme INTEGER,
+                    password TEXT,    
+                    fontSize INTEGER,
+                    showMiniCalendar INTEGER,
+                    notesShowInterval INTEGER,
+                    lang TEXT,
+                    calendarNotesCounter INTEGER
+                );
+            `);
+            await execureSQL(`
+                INSERT INTO Settings (defaultNotification, fastAdd, theme, password, fontSize) 
+                SELECT defaultNotification, fastAdd, colorTheme AS theme, password, fontSize 
+                FROM Settings_OLD;
+            `);
+            await execureSQL(`DROP TABLE Settings_OLD;`);
+            let sort = {
+                type: 1,
+                direction: 1,
+                finSort: 0
+            }
+            let lang = navigator.globalization ? (await new Promise((resolve, reject) => navigator.globalization.getPreferredLanguage(resolve, reject))) : config.defaultLang;
+            if (lang) {
+                lang = lang.value || "en";
+            }
+            if (lang.indexOf("-") !== -1) {
+                lang = lang.split("-")[0];
+            }
+            await execureSQL(`
+                UPDATE Settings 
+                SET 
+                    sort = ?, 
+                    showMiniCalendar = ?,
+                    notesShowInterval = ?,
+                    lang = ?,
+                    calendarNotesCounter = ?;`, 
+                [JSON.stringify(sort), 1, 1, lang, 1]
+            );
+        }
+
+        async function alterTasksTable () {
+            let msNow = +new Date();
+            await execureSQL(`ALTER TABLE Tasks RENAME TO Tasks_OLD;`);
+            await execureSQL(`                           
+                CREATE TABLE IF NOT EXISTS Tasks
+                (
+                    id INTEGER PRIMARY KEY,
+                    uuid TEXT,
+                    title TEXT,
+                    startTime INTEGER,
+                    endTime INTEGER,
+                    notificate INTEGER,
+                    tag TEXT,
+                    dynamicFields TEXT,
+                    added INTEGER,
+                    finished INTEGER DEFAULT 0,
+                    isSynced INTEGER DEFAULT 0,
+                    isLastActionSynced INTEGER DEFAULT 0,
+                    lastAction TEXT,
+                    lastActionTime INTEGER,
+                    userId INTEGER,
+                    repeatType INTEGER DEFAULT "no-repeat",
+                    UNIQUE (uuid) ON CONFLICT REPLACE 
+                );
+            `);
+            console.log(await execureSQL(`
             SELECT
-                id, 
-                uuid, 
+                id,  
                 title, 
                 startTime, 
                 endTime, 
@@ -119,17 +117,44 @@ export default {
                 ? as lastActionTime,
                 null as userId,
                 'no-repeat' as repeatType
-            FROM Settings_OLD;
-        `, [msNow]);
-        await execureSQL(`DROP TABLE Settings_OLD;`);
-
-        await execureSQL(
-            `CREATE TABLE IF NOT EXISTS TasksRepeatValues
-            (
-                taskId INTEGER,
-                value INTEGER,
-                FOREIGN KEY(taskId) REFERENCES Tasks(id)
-            );`
-        )
+            FROM Tasks_OLD;`, [msNow]));
+            await execureSQL(`
+                INSERT INTO Tasks (
+                    id, 
+                    title, 
+                    startTime, 
+                    endTime, 
+                    notificate, 
+                    tag, 
+                    dynamicFields, 
+                    added, 
+                    finished, 
+                    isSynced,
+                    isLastActionSynced,
+                    lastAction,
+                    lastActionTime,
+                    userId,
+                    repeatType
+                ) 
+                SELECT
+                    id,  
+                    title, 
+                    startTime, 
+                    endTime, 
+                    notificate, 
+                    tag, 
+                    dynamicFields, 
+                    added, 
+                    finished, 
+                    0 as isSynced,
+                    0 as isLastActionSynced,
+                    'ADD' as lastAction,
+                    ? as lastActionTime,
+                    null as userId,
+                    'no-repeat' as repeatType
+                FROM Tasks_OLD;
+            `, [msNow]);
+            await execureSQL(`DROP TABLE Tasks_OLD;`);
+        }
     }
 }
