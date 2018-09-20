d1a69d40002774013ec5f812af48dac0b5963c1e
Vadim Semenyuk <vadim54787@gmail.com>
2018-09-20 18:28:50 +0300

    calendar count refactor

--- app/src/services/calendar.service.js
+++ app/src/services/calendar.service.js
@@ -12,14 +12,24 @@ class CalendarService {
         let intervalEndDate = moment(date).startOf(period).add(halfInterval, period).valueOf();
 
         let selectTask = executeSQL(
-            `SELECT COUNT(*) as count, added FROM Tasks
-            WHERE 
-                added >= ? AND added <= ? AND 
-                userId = ? AND 
-                lastAction != 'DELETE' AND
-                repeatType = 'no-repeat'
+            `SELECT COUNT(*) as count, added FROM (
+                SELECT added FROM Tasks
+                            WHERE
+                                added >= ? AND added <= ? AND
+                                userId = ? AND
+                                lastAction != 'DELETE' AND
+                                repeatType = 'no-repeat'
+                UNION ALL
+                SELECT rep.value as added FROM Tasks t
+                            LEFT JOIN TasksRepeatValues rep ON t.id = rep.taskId
+                            WHERE
+                                rep.value >= ? AND rep.value <= ? AND
+                                userId = ? AND
+                                lastAction != 'DELETE' AND
+                                t.repeatType = 'any'
+                )
             GROUP BY added;`, 
-            [intervalStartDate, intervalEndDate, authService.getUserId()]
+            [intervalStartDate, intervalEndDate, authService.getUserId(), intervalStartDate, intervalEndDate, authService.getUserId()]
         );  
 
         let selectRepeatableDayTask = executeSQL(
@@ -40,26 +50,13 @@ class CalendarService {
                 repeatType = "week"
 			GROUP BY rep.value;`, 
             [authService.getUserId()]
-        );    
+        );      
 
-        let selectRepeatableAnyTask = executeSQL(
-            `SELECT COUNT(*) as count, rep.value as added FROM Tasks t
-			LEFT JOIN TasksRepeatValues rep ON t.id = rep.taskId
-            WHERE
-                rep.value >= ? AND rep.value <= ? AND 
-                userId = ? AND
-                lastAction != 'DELETE' AND
-				t.repeatType = 'any'
-			GROUP BY rep.value;`, 
-            [intervalStartDate, intervalEndDate, authService.getUserId()]
-        );  
-
-        let selects = await Promise.all([selectTask, selectRepeatableDayTask, selectRepeatableWeekTask, selectRepeatableAnyTask]);
+        let selects = await Promise.all([selectTask, selectRepeatableDayTask, selectRepeatableWeekTask]);
 
         let select = selects[0];
         let selectRepeatableDay = selects[1];
         let selectRepeatableWeek = selects[2];
-        let selectRepeatableAny = selects[3];
 
         let count = {};
         for (let i = 0; i < select.rows.length; i++) {
@@ -76,10 +73,6 @@ class CalendarService {
         for (let i = 0; i < selectRepeatableWeek.rows.length; i++) {
             let countItem = selectRepeatableWeek.rows.item(i);
             repeatable.week[countItem.weekDay] = countItem.count
-        }
-        for (let i = 0; i < selectRepeatableAny.rows.length; i++) {
-            let countItem = selectRepeatableAny.rows.item(i);
-            repeatable.any[countItem.added] = countItem.count
         }
 
         return {
