325291b96356271286b57a18c98869a1a0dadf45
Vadim Semenyuk <vadim54787@gmail.com>
2018-10-18 00:11:27 +0300

    config added

---
+++ server/server/config.js
@@ -0,0 +1,23 @@
+const ENV = process.env.NODE_ENV || 'DEVELOPMENT';
+
+const config = {
+    DEVELOPMENT: {
+        db: {
+            user: process.env.DB_USER || 'postgres',
+            host: process.env.DB_HOST || 'localhost',
+            database: process.env.DB_DATABASE || 'dbname',
+            password: process.env.DB_PASSWORD || '1234',
+            port: process.env.DB_PORT || '5432'
+        },
+        jwtSecret: process.env.JWT_SECRET || '$doyoureallywannaknow',  
+        webClientId: process.env.WEB_CLIENT_ID || "390152836612-5rcub6svcoico5lterd17i6fog3pg9lg.apps.googleusercontent.com",
+        telegramToken: process.env.TELEGRAM_TOKEN,
+        httpPort: process.env.HTTP_PORT || '3001',
+        httpsPort: process.env.HTTPS_PORT || '3001',  
+        allowHttps: process.env.ALLOW_HTTPS || 'false',
+        sslKeyPath: process.env.SSL_KEY_PATH,
+        sslCertPath: process.env.SSL_CERT_PATH,
+    },
+};
+
+module.exports = config[ENV];
