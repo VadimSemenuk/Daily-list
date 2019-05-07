160bd5782329fee9ab0fcfd9e78756208296e7c3
Vadim Semenyk <vadim54787@gmail.com>
2019-05-07 06:49:18 +0300

    logs fix

--- server/server/routes/log.js
+++ server/server/routes/log.js
@@ -2,7 +2,7 @@ const router = require('express-promise-router')();
 
 module.exports = function (logRep) {  
     router.post('/load', (req, res, next) => {
-        logRep.logLoad(req.body.deviceId);
+        logRep.logLoad(req.body);
         res.end(); 
     });
 
