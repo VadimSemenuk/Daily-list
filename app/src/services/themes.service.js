b5ff5eebae16da169a4901e3d36c9f969051b401
Vadim Semenyuk <vadim54787@gmail.com>
2021-07-25 16:48:03 +0300

    remove opacity from tag active color

--- app/src/services/themes.service.js
+++ app/src/services/themes.service.js
@@ -6,76 +6,91 @@ class ThemesService {
             id: 0,
             header: '#00213C',
             body: '#fff',
+            light: '#9bacb9'
         },
         {
             id: 1,
             header: '#F44336',
             body: '#fff',
+            light: '#f79e97'
         },
         {
             id: 2,
             header: '#E91E63',
             body: '#fff',
+            light: '#e092ac'
         },
         {
             id: 3,
             header: '#9C27B0',
             body: '#fff',
+            light: '#d09ed8'
         },
         {
             id: 4,
             header: '#00BCD4',
             body: '#fff',
+            light: '#8fd5de'
         },
         {
             id: 5,
             header: '#009688',
             body: '#fff',
+            light: '#85c3be'
         },
         {
             id: 6,
             header: '#4CAF50',
             body: '#fff',
+            light: '#9bca9d'
         },
         {
             id: 7,
             header: '#8BC34A',
             body: '#fff',
+            light: '#b9d895'
         },
         {
             id: 8,
             header: '#CDDC39',
             body: '#fff',
+            light: '#d5dc8a'
         },
         {
             id: 9,
             header: "#f57c00",
             body: '#fff',
+            light: '#ecbb88'
         },
         {
             id: 10,
             header: '#795548',
             body: '#fff',
+            light: '#c19889'
         },
         {
             id: 11,
             header: '#6c6c6c',
             body: '#fff',
+            light: '#afafaf'
         },
         {
             id: 12,
             header: '#607D8B',
             body: '#fff',
+            light: '#aabec7'
         },
         {
             id: 13,
             header: '#222222',
             body: '#fff',
+            light: '#afafaf'
         },
         {
             id: 14,
             header: '#2b3b46',
             body: '#fff',
+            light: '#9dbace'
         }
     ];
 
@@ -126,7 +141,7 @@ class ThemesService {
                     background: ${theme.header};
                 }
                 .theme-tag {
-                    background: ${theme.header}60;
+                    background: ${theme.light};
                 }
             `;
         }
