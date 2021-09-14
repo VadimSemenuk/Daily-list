788605a1b69ddcc9abf301f130530a7c10edaa82
Vadim Semenyuk <vadim54787@gmail.com>
2021-09-14 18:18:53 +0300

    auto dash on fast add

--- app/src/components/QuickAdd/QuickAdd.js
+++ app/src/components/QuickAdd/QuickAdd.js
@@ -39,6 +39,64 @@ class QuickAdd extends PureComponent {
         }
     }
 
+    updateValue = (event) => {
+        let moveCaretToPosition = null;
+        let nextValue = event.target.value;
+
+        if (this.isNewRowAdded(this.state.value, nextValue) && event) {
+            let rows = this.state.value.split("\n");
+            let cursorPos = event.target.selectionStart;
+
+            let charCounter = 0;
+            let targetRowIndex = null;
+            for (let i = 0; i < rows.length; i++) {
+                let row = rows[i];
+
+                charCounter += row.length + 1;
+                if (charCounter >= cursorPos) {
+                    targetRowIndex = i;
+                    break;
+                }
+            }
+
+            if (targetRowIndex !== null) {
+                if (rows[targetRowIndex][0] === "-") {
+                    let nextTargetRow = rows[targetRowIndex];
+                    if (/^-\S+/.test(nextTargetRow)) {
+                        nextTargetRow = nextTargetRow.replace("-", "- ");
+                    } else {
+                        nextTargetRow = nextTargetRow.replace(/^-\s+/, "- ");
+                    }
+                    nextTargetRow += "\n- ";
+
+                    let _nextValue = [
+                        ...rows.slice(0, targetRowIndex),
+                        nextTargetRow,
+                        ...rows.slice(targetRowIndex + 1)
+                    ].join("\n");
+                    nextValue = _nextValue;
+
+                    let nextCursorPosition = [
+                        ...rows.slice(0, targetRowIndex),
+                        nextTargetRow
+                    ].reduce((acc, item) => acc += item.length + 1, 0) - 1;
+                    moveCaretToPosition = nextCursorPosition;
+                }
+            }
+        }
+
+        this.setState({value: nextValue});
+
+        if (moveCaretToPosition !== null) {
+            setTimeout(() => {
+                let el = document.querySelector(".quick-add textarea");
+                el && el.setSelectionRange(moveCaretToPosition, moveCaretToPosition);
+            });
+        }
+    }
+
+    isNewRowAdded = (prevValue, value) => prevValue.split("\n").length < value.split("\n").length
+
     add = () => {
         let note = this.getDefaultNoteData();
         note.tags = this.props.settings.noteFilters.tags.map((id) => this.props.tags.find((tag) => tag.id === id));
@@ -62,7 +120,7 @@ class QuickAdd extends PureComponent {
                     type="text"
                     placeholder={t("quick-add-placeholder")}
                     value={this.state.value}
-                    onChange={(e) => this.setState({value: e.target.value})}
+                    onChange={this.updateValue}
                 />
 
                 <div className="actions-wrapper">
