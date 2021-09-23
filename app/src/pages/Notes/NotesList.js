c97fffbb7d0ddff463a809a55ffe0887944e5217
Vadim Semenyuk <vadim54787@gmail.com>
2021-09-23 20:24:13 +0300

    fixes

--- app/src/pages/Notes/NotesList.js
+++ app/src/pages/Notes/NotesList.js
@@ -77,7 +77,7 @@ class NotesList extends PureComponent {
         let sortableOptions = {
             options: {
                 disabled: this.props.settings.sortType === SortType.TimeSort,
-                delay: 500,
+                delay: 300,
                 draggable: ".draggable",
                 handle: ".drag-handle",
                 direction: 'vertical',
