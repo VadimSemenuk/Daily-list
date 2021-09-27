7a44aefc8af589969dd06964ba53ce5fd5866e5d
Vadim Semenyuk <vadim54787@gmail.com>
2021-09-27 15:06:40 +0300

    dates update

--- app/src/db/migration/migrations/index.js
+++ app/src/db/migration/migrations/index.js
@@ -3,11 +3,13 @@ import fromLocalStorage from "./from-local-storage";
 import v16 from "./v16";
 import v17 from "./v17";
 import v18 from "./v18";
+import v19 from "./v19";
 
 export default [
     v16,
     v17,
-    v18
+    v18,
+    v19
 ];
 
 export {init, fromLocalStorage};
